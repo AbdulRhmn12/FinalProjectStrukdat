fb3bbe1a8f1fc7dd26ead2db5a5dc421514f8000
Maisie Yunita Malva <121386943+MaisieYunitaMalva@users.noreply.github.com>
2022-12-26 14:52:37 +0700

    Deklarasi fungsi data isEmpty

---
+++ isEmpty.cpp
@@ -0,0 +1,23 @@
+bool isBajuEmpty(){
+	if(head==NULL){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool isAmbilEmpty(){
+	if(top1==NULL){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool isTerambilEmpty(){
+	if(head1==NULL){
+		return true;
+	}else{
+		return false;
+	}
+}
